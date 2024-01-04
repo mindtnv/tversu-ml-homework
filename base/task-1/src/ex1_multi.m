b095c19647882238dad896a588eabc015c4a93c7
midntnv <mindtnv@gmail.com>
2024-01-04 16:48:31 +0300

    task 1

--- base/task-1/src/ex1_multi.m
+++ base/task-1/src/ex1_multi.m
@@ -56,8 +56,8 @@ X = [ones(m, 1) X];
 fprintf('Проводим градиентный спуск ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 0.125;
+num_iters = 40;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -80,6 +80,7 @@ fprintf('\n');
 % его не требуется нормализовать.
 price = 0; % Вам нужно изменить это значение
 
+price = [1, (1650 - mu(1)) / sigma(1), (3 - mu(2)) / sigma(2)] * theta;
 
 % ====================== КОНЕЦ ВАШЕГО КОДА ======================
 
@@ -124,7 +125,7 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-mt, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+price = [1, 1650, 3] * theta;
 
 % ============================================================
 
